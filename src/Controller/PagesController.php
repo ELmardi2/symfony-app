d945a2907fea2503602f784cc441f1de45c560bc
Elmardi2 <mardi.yahia@gmail.com>
2018-09-22 20:24:24 +0200

    Initial commit

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/pages", name="pages")
+     */
+    public function index()
+    {
+        return $this->render('pages/index.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this->render('pages/home.html.twig');
+    }
+}

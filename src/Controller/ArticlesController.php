e8876f14987303fe9fda1c95763f9f1582de7f2f
Elmardi2 <mardi.yahia@gmail.com>
2018-09-26 17:46:04 +0200

    recover articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -5,21 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class ArticlesController extends AbstractController
 {
     /**
-     * @Route("/articles", name="articles")
+     * @Route("articles", name="articles")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
        //#call my repository to have all what he has 
-        $repo = $this->getDoctrine()->getRepository(Article::class);
+        //$repo = $this->getDoctrine()->getRepository(Article::class);
           //#find the articles as i want all, some, orderBy ...etc#
            $articles = $repo->findAll();
         return $this->render('articles/index.html.twig', [
             'controller_name' => 'ArticleController',
             'articles' =>$articles,
+        ]);
+    }
+      /**
+     * @Route("/articles/{id}", name="articles_show")
+     */
+    public function show(Article $article)
+    {
+        return $this->render('/articles/show.html.twig', [
+            'article' =>$article,
         ]);
     }
 }
